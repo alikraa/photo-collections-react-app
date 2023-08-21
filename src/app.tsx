23dc6c347922f83bc8088ea43e6a08cb8e8e0081
Alina <krakhmaalina@gmail.com>
2023-08-21 18:17:44 +0300

    app layout added

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,62 @@
-import './app.css';
+import './app.scss';
 
 function App() {
-  return <></>;
+  return (
+    <div className="photo-collections-app">
+      <h1 className="header">Моя коллекция фотографий</h1>
+      <div className="photo-collections-app__content">
+        <div className="categories">
+          <ul>
+            <li>Все</li>
+            <li>Горы</li>
+            <li>Море</li>
+            <li>Архитектура</li>
+            <li>Города</li>
+          </ul>
+          <input type="text" name="" id="" placeholder="Поиск по названию" />
+        </div>
+        <div className="collections">
+          <div className="collection">
+            <img src="./img-icon.svg" alt="Img" id="1" className="img-1" />
+            <img src="./img-icon.svg" alt="Img" id="2" />
+            <img src="./img-icon.svg" alt="Img" id="3" />
+            <img src="./img-icon.svg" alt="Img" id="4" />
+            <h4>Путешествия по миру</h4>
+          </div>
+          <div className="collection">
+            <img src="./img-icon.svg" alt="Img" id="1" className="img-1" />
+            <img src="./img-icon.svg" alt="Img" id="2" />
+            <img src="./img-icon.svg" alt="Img" id="3" />
+            <img src="./img-icon.svg" alt="Img" id="4" />
+            <h4>Путешествия по миру</h4>
+          </div>
+          <div className="collection">
+            <img src="./img-icon.svg" alt="Img" id="1" className="img-1" />
+            <img src="./img-icon.svg" alt="Img" id="2" />
+            <img src="./img-icon.svg" alt="Img" id="3" />
+            <img src="./img-icon.svg" alt="Img" id="4" />
+            <h4>Путешествия по миру</h4>
+          </div>
+          <div className="collection">
+            <img src="./img-icon.svg" alt="Img" id="1" className="img-1" />
+            <img src="./img-icon.svg" alt="Img" id="2" />
+            <img src="./img-icon.svg" alt="Img" id="3" />
+            <img src="./img-icon.svg" alt="Img" id="4" />
+            <h4>Путешествия по миру</h4>
+          </div>
+        </div>
+        <button type="button" className="pagination">
+          1
+        </button>
+        <button type="button" className="pagination">
+          2
+        </button>
+        <button type="button" className="pagination">
+          3
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app.tsx';
-import './index.css';
+import './_reset.scss';
+import './_index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
